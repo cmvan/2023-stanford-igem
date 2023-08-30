053bc8bdbba4e452ada0bb54564cd765abcb9c39
Caroline Van <cmvan@stanford.edu>
2023-08-30 02:09:18 -0700

    configure browser router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Description from './components/pages/Description';
 import Home from './components/pages/Home';
 import Design from './components/pages/Design';
@@ -26,30 +26,28 @@ import Awards from './components/pages/Awards';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/description" element={<Description />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/design" element={<Design />} />
-        <Route path="/engineering" element={<Engineering />} />
-        <Route path="/implementation" element={<Implementation />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/partcollection" element={<PartCollection />} />
-        <Route path="/parts" element={<Parts />} />
-        <Route path="/experiments" element={<Experiments />} />
-        <Route path="/safety" element={<Safety />} />
-        <Route path="/notebook" element={<Notebook />} />
-        <Route path="/integratedhumanpractices" element={<HumanPractices />} />
-        <Route path="/communication" element={<Communication />} />
-        <Route path="/sustainabledevelopment" element={<SustainableDevelopment />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/ourteam" element={<Team />} />
-        <Route path="/attributions" element={<Attributions />} />
-        <Route path="/ourcontribution" element={<Contribution />} />
-        <Route path="/awards" element={<Awards />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/description" element={<Description />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/design" element={<Design />} />
+      <Route path="/engineering" element={<Engineering />} />
+      <Route path="/implementation" element={<Implementation />} />
+      <Route path="/results" element={<Results />} />
+      <Route path="/partcollection" element={<PartCollection />} />
+      <Route path="/parts" element={<Parts />} />
+      <Route path="/experiments" element={<Experiments />} />
+      <Route path="/safety" element={<Safety />} />
+      <Route path="/notebook" element={<Notebook />} />
+      <Route path="/integratedhumanpractices" element={<HumanPractices />} />
+      <Route path="/communication" element={<Communication />} />
+      <Route path="/sustainabledevelopment" element={<SustainableDevelopment />} />
+      <Route path="/education" element={<Education />} />
+      <Route path="/ourteam" element={<Team />} />
+      <Route path="/attributions" element={<Attributions />} />
+      <Route path="/ourcontribution" element={<Contribution />} />
+      <Route path="/awards" element={<Awards />} />
+    </Routes>
   );
 }
 
