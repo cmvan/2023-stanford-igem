49eab974c82d17f170043e7708575363792c8a8d
ngochtran <ngochtran2015@gmail.com>
2023-10-11 18:45:45 -0700

    Template Added to Each Page

--- src/components/pages/PartCollection.js
+++ src/components/pages/PartCollection.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import Template from '../Template'
+import { useState } from 'react';
+import TableOfContents from '../TableOfContents';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 const PartCollection = () => {
+    const [key, setKey] = useState('overview');
     return ( 
         <Template title="Part Collection">
-            <h1> Hello </h1>
+            <div className="row"> 
+                <div className="toc-index col-sm-2"> 
+                    <TableOfContents activeKey={key}/>
+                </div>
+                <div className="tab-container col-lg">
+                    <Tabs
+                        id="controlled-tab-example"
+                        activeKey={key}
+                        onSelect={(k) => setKey(k)}
+                        className="mb-3"
+                        >
+                        <Tab eventKey="overview" title="Overview">
+                            <section id="overview">
+                                <h2 className="content-section" id="experiments-MH-1">Molecular Biology Protocols</h2>
+                            </section>
+                        </Tab>
+                    </Tabs>
+                    </div>
+                </div>
         </Template>
     )
 }
